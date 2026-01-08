99255184701825112b6fc950fbb02369a19e94dd
NASRALLAH-DAMMAJ <nasrallahdammaj@gmail.com>
2026-01-08 16:13:36 +0300

    PS2: Solve camel.py

---
+++ week2/camel/camel.py
@@ -0,0 +1,16 @@
+def main():
+    camel_case = input("camelCase: ")
+    snake_case = ""
+
+    for c in camel_case:
+        if c.isupper():
+            snake_case += "_"+ c.lower()
+        
+        else:
+            snake_case += c
+    
+    print("snake_case:", snake_case)
+    
+
+if __name__ == "__main__":
+    main()
