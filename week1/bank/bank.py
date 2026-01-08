690028c663b1b9d30fed21af9a6b41a0c9e7d9c6
NASRALLAH-DAMMAJ <nasrallahdammaj@gmail.com>
2026-01-08 14:55:57 +0300

    PS1: Solve bank.py

---
+++ week1/bank/bank.py
@@ -0,0 +1,14 @@
+def main():
+    text = input("Greeting: ").strip().lower()
+
+    if text.startswith("hello"):
+        print("$0")
+
+    elif text.startswith("h"):
+        print("$20")
+
+    else:
+        print("$100")
+
+
+main()
